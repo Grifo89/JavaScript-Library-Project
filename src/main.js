be8ee0b14a3e2023eb4895f69e8f5ea79ac0b6dd
viricruz <viridiana.cruz.olivera@gmail.com>
2020-02-11 12:58:28 -0700

    Add switchStatus function, now renders every book in the body

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ let myLibrary = []
 function Book(author, title, pages, read) {
   this.author = author;
   this.title = title;
-  this.page = pages;
+  this.pages = pages;
   this.read = read;
 }
 
-function addBookToLibrary(book) {
-  return myLibrary.push(book)
+function addBookToLibrary() {
+  let book = new Book(
+    author = document.getElementsByName('author')[0].value,
+    title = document.getElementsByName('title')[0].value,
+    pages = document.getElementsByName('pages')[0].value,
+    read = false
+  );
+  myLibrary.push(book)
+  render()
 }
 
 function render() {
@@ -17,19 +24,46 @@ function render() {
   let author = document.createElement("span")
   let pages = document.createElement("span")
   let button = document.createElement("button")
+  button.addEventListener('click', (e) => {
+    switchStatus(e)
+  })
+  let remove = document.createElement('button')
   let body = document.querySelector("body")
   myLibrary.forEach((item, i) => {
-    div.appendChild(title.innerHTML = item.title)
-    div.appendChild(author.innerHTML = item.author)
-    div.appendChild(pages.innerHTML = item.pages)
-    div.appendChild(button.innerHTML = "Status").setAttribute("rel", i);
-    body.appendChild(div)
+    title.innerHTML = item.title
+    author.innerHTML = item.author
+    pages.innerHTML = item.pages
+    button.innerHTML = "Status"
+    button.setAttribute('class', 'status')
+    remove.innerHTML = 'Remove'
+    remove.setAttribute('class', 'remove')
+    div.appendChild(title).setAttribute('class', 'title')
+    div.appendChild(author).setAttribute('class', 'author')
+    div.appendChild(pages).setAttribute('class', 'pages')
+    div.appendChild(button).setAttribute("rel", i)
+    div.appendChild(remove).setAttribute('rel', i)
+    
+    body.appendChild(div).setAttribute('class', 'book-container')
   });
 }
 
-function switchStatus() {
-  addEventListener("click", (e) => {
+function setStatus(read, button) {
+  if (read) {
+    button.innerText = 'Read'
+  } else {
+    button.innerText = 'Not read yet'
+  }
+}
+
+function switchStatus(e) {
+  let index = e.srcElement.attributes["1"].value
+  myLibrary[parseInt(index)].read = !myLibrary[parseInt(index)].read
+  setStatus(myLibrary[parseInt(index)].read, e.target)
+}
+
+function removeBook() {
+  addEventListener('click', (e) => {
     let index = e.srcElement.attributes["3"].value
-    myLibrary[parseInt(index)][read] = true
+    delete myLibrary[index]
   })
 }
