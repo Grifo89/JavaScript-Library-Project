328076bd195230e7ad4c87d81e55afc1353254b0
Christian Salazar <calaca89@gmail.com>
2020-02-11 15:53:05 -0500

    Finish the books library project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-let myLibrary = []
+const myLibrary = [];
 
 function Book(author, title, pages, read) {
   this.author = author;
@@ -8,62 +8,71 @@ function Book(author, title, pages, read) {
 }
 
 function addBookToLibrary() {
-  let book = new Book(
+  const book = new Book(
     author = document.getElementsByName('author')[0].value,
     title = document.getElementsByName('title')[0].value,
     pages = document.getElementsByName('pages')[0].value,
-    read = false
+    read = false,
   );
-  myLibrary.push(book)
-  render()
+  myLibrary.push(book);
+  document.getElementsByName('author')[0].value = '';
+  document.getElementsByName('title')[0].value = '';
+  document.getElementsByName('pages')[0].value = '';
+  render();
 }
 
 function render() {
-  let div = document.createElement("div")
-  let title = document.createElement("h3")
-  let author = document.createElement("span")
-  let pages = document.createElement("span")
-  let button = document.createElement("button")
+  const div = document.createElement('div');
+  const title = document.createElement('h3');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const button = document.createElement('button');
   button.addEventListener('click', (e) => {
-    switchStatus(e)
-  })
-  let remove = document.createElement('button')
-  let body = document.querySelector("body")
+    switchStatus(e);
+  });
+  const remove = document.createElement('button');
+  remove.addEventListener('click', (e) => {
+    removeBook(e);
+  });
+  const body = document.querySelector('.flex');
   myLibrary.forEach((item, i) => {
-    title.innerHTML = item.title
-    author.innerHTML = item.author
-    pages.innerHTML = item.pages
-    button.innerHTML = "Status"
-    button.setAttribute('class', 'status')
-    remove.innerHTML = 'Remove'
-    remove.setAttribute('class', 'remove')
-    div.appendChild(title).setAttribute('class', 'title')
-    div.appendChild(author).setAttribute('class', 'author')
-    div.appendChild(pages).setAttribute('class', 'pages')
-    div.appendChild(button).setAttribute("rel", i)
-    div.appendChild(remove).setAttribute('rel', i)
-    
-    body.appendChild(div).setAttribute('class', 'book-container')
+    title.innerHTML = `Title: ${item.title}`;
+    author.innerHTML = `Author: ${item.author}`;
+    pages.innerHTML = `Number of pages: ${item.pages}`;
+    button.innerHTML = 'Status';
+    button.setAttribute('class', 'status');
+    remove.innerHTML = 'Remove';
+    remove.setAttribute('class', 'remove');
+    div.appendChild(title).setAttribute('class', 'title');
+    div.appendChild(author).setAttribute('class', 'author');
+    div.appendChild(pages).setAttribute('class', 'pages');
+    div.appendChild(button).setAttribute('rel', i);
+    div.appendChild(remove).setAttribute('rel', i);
+
+    body.appendChild(div).setAttribute('class', 'book-container');
   });
 }
 
 function setStatus(read, button) {
   if (read) {
-    button.innerText = 'Read'
+    button.innerText = 'Read';
   } else {
-    button.innerText = 'Not read yet'
+    button.innerText = 'Not read yet';
   }
 }
 
 function switchStatus(e) {
-  let index = e.srcElement.attributes["1"].value
-  myLibrary[parseInt(index)].read = !myLibrary[parseInt(index)].read
-  setStatus(myLibrary[parseInt(index)].read, e.target)
+  const index = e.srcElement.attributes['1'].value;
+  myLibrary[parseInt(index)].read = !myLibrary[parseInt(index)].read;
+  setStatus(myLibrary[parseInt(index)].read, e.target);
 }
 
-function removeBook() {
-  addEventListener('click', (e) => {
-    let index = e.srcElement.attributes["3"].value
-    delete myLibrary[index]
-  })
+function removeBook(e) {
+  const body = document.querySelector('.flex');
+  const child = e.target.parentNode;
+  if (child && myLibrary.length !== 0) {
+    body.removeChild(child);
+    const index = e.srcElement.attributes['1'].value;
+    delete myLibrary[index];
+  }
 }
